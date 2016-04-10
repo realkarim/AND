8c21f27587f26cd31b59f169bbf50f680b382072
Karim <karim.mostafa.18@gmail.com>
2016-04-11 01:15:54 +0200

    Create the main UI: Title + button for every app!

---
+++ app/src/main/java/apps/realkarim/myappportfolio/MainActivity.java
@@ -0,0 +1,90 @@
+package apps.realkarim.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button popular_movies = (Button) findViewById(R.id.popular_movies);
+        Button stack_hawk = (Button) findViewById(R.id.stack_hawk);
+        Button build_it_bigger = (Button) findViewById(R.id.build_it_bigger);
+        Button make_your_app_material = (Button) findViewById(R.id.make_your_app_material);
+        Button go_ubiquitous = (Button) findViewById(R.id.go_ubiquitous);
+        Button capstone = (Button) findViewById(R.id.capstone);
+
+        popular_movies.setOnClickListener(this);
+        stack_hawk.setOnClickListener(this);
+        build_it_bigger.setOnClickListener(this);
+        make_your_app_material.setOnClickListener(this);
+        go_ubiquitous.setOnClickListener(this);
+        capstone.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return false;                                                   // set to true if I want to show the menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.popular_movies:
+                Toast.makeText(context,"This button will launch my popular movies app!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.stack_hawk:
+                Toast.makeText(context,"This button will launch my stack hawk app!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.build_it_bigger:
+                Toast.makeText(context,"This button will launch my build it bigger app!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.make_your_app_material:
+                Toast.makeText(context,"This button will launch my make your app material app!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.go_ubiquitous:
+                Toast.makeText(context,"This button will launch my go ubiquitous app!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.capstone:
+                Toast.makeText(context,"This button will launch my capstone app!",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
